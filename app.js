c0ddb9e5f4f6a7dbf5aa80674148268fe9bb8a9b
Rafzy <khoirulloh.rafi2@gmail.com>
2021-07-16 15:18:12 +0700

    Basic setup nodejs

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
 const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const favicon = require('serve-favicon')
+const cookieParser = require('cookie-parser')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const swaggerUI = require('swagger-ui-express')
+const session = require('express-session')
+const apiDocumentation = require('./api/api-docs')
 const app = express()
+
+// Basic Set-up
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+app.use(fileUpload())
+app.use(helmet.frameguard())
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
+app.disable('x-powered-by')
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('dev'))
+
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'develop'
+  // store: new SequelizeStore({ db: Database, table: 'Session', extendDefaultFields })
+}))
+
+// Api Documentation
+const options = {
+  customCss: '.swagger-ui .topbar {display: none}'
+}
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(apiDocumentation, options))
 
 app.get('/', (req, res) => {
   res.json({
     message: 'testing'
+  })
+})
+
+// Error Handling
+app.get('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: '404 Not Found'
   })
 })
 
