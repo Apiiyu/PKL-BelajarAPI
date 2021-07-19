a944dd6ef50c33d12d718eef40419afe345eb4b5
Rafzy <khoirulloh.rafi2@gmail.com>
2021-07-19 12:41:25 +0700

    Create controller & restful api

---
+++ controller/ProductController.js
@@ -0,0 +1,30 @@
+const Model = require('../config/model/Index')
+const controller = {}
+
+controller.getAllData = async (req, res) => {
+  try {
+    Model.Product.findAll()
+      .then((result) => {
+        if (result.length > 0) {
+          res.status(200).json({
+            status: 200,
+            message: 'Successfully get all data product!',
+            data: result
+          })
+        } else {
+          res.status(200).json({
+            status: 200,
+            message: 'Data not found',
+            data: []
+          })
+        }
+      })
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      message: error
+    })
+  }
+}
+
+module.exports = controller
